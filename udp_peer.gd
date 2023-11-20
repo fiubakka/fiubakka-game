7806292fad95e14d4edde95379b6ceff9333da56
Franco Scaccheri <fscaccheri@gmail.com>
2023-11-20 16:07:44 -0300

    WIP: change UDP peer for TCP peer

--- udp_peer.gd
+++ udp_peer.gd
@@ -6,20 +6,22 @@ signal create_other_player
 
 const host = "127.0.0.1"
 const port = 8000
-var socket = PacketPeerUDP.new()
+#var socket = PacketPeerUDP.new()
+var socket = StreamPeerTCP.new()
 var connected = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	socket.connect_to_host(host, port)
+	var error = socket.connect_to_host(host, port)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if socket.get_available_packet_count() > 0:
-		var server_pkt = socket.get_packet().get_string_from_utf8()
+	if socket.get_available_bytes() > 0:
+		var server_str = socket.get_string()
 		
-		var msg = server_pkt.split(" ")
+		var msg = server_str.split(" ")
 		match msg[0]:
 			"POS":
 				var position = Vector2(float(msg[1]), float(msg[2]))
@@ -39,12 +41,12 @@ func _process(delta):
 
 func init_pos(position, screen_size):
 	print("mandando la pos")
-	socket.put_packet(str("INIT ", position.x, " ", position.y, " ", screen_size.x, " ", screen_size.y).to_utf8_buffer())
+	socket.put_string(str("INIT ", position.x, " ", position.y, " ", screen_size.x, " ", screen_size.y))
 
 
 func change_velocity(vel):
-	socket.put_packet(str("VEL ", vel.x, " ", vel.y).to_utf8_buffer())
+	socket.put_string(str("VEL ", vel.x, " ", vel.y))
 
 
 func _on_main_change_velocity(vel):
-	socket.put_packet(str("VEL ", vel.x, " ", vel.y).to_utf8_buffer())
+	socket.put_data(str("VEL a", vel.x, " ", vel.y).to_ascii_buffer())
