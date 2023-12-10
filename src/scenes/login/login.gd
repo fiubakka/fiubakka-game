809049f294ec352c463336cd82bf0b94548626e8
FScaccheri <fscaccheri@gmail.com>
2023-12-10 19:22:29 -0300

    Move main scen into its own file

--- src/scenes/login/login.gd
+++ src/scenes/login/login.gd
@@ -10,7 +10,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (Input.is_action_just_pressed("open_chat")): # open_chat == Y
+	if (Input.is_action_just_pressed("force_login")): # force_login == L
 		init_tcp_peer.emit("flu")
 		change_to_zone_scene.emit()
 		get_node(".").free()
