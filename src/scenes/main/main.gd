0b9d9875c83823f5d4a4bc29dd248e997b6cee7d
Ivan Soriano <isoriano@fi.uba.ar>
2023-12-12 18:42:37 -0300

    Add username label for other players

--- src/scenes/main/main.gd
+++ src/scenes/main/main.gd
@@ -4,7 +4,9 @@ extends Node
 @export var zone_scene: PackedScene
 
 var other_players = {}
+var other_players_label = {}
 var screen_size
+const pattern = "^Player-(.*)$"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -34,6 +36,8 @@ func _on_tcp_peer_update_other_player_pos(id, position, velocity):
 		var other_player = other_players[id]
 		other_player.position = position
 		other_player.velocity = velocity
+		var label = other_players_label[id]
+		label.position = Vector2(other_player.position.x - 35, other_player.position.y - 100)
 	else:
 		var other_player = other_placer_scene.instantiate()
 		other_player.id = id
@@ -41,7 +45,23 @@ func _on_tcp_peer_update_other_player_pos(id, position, velocity):
 		other_player.velocity = velocity
 		other_players[id] = other_player
 		add_child(other_player)
+		var nameLabel = Label.new()
+		nameLabel.text = get_username_from_id(other_player.id) # Because id is like Player-username and we want to show only the name
+		nameLabel.position = Vector2(other_player.position.x - 35, other_player.position.y - 100) # There must be a way to handle the position dynamically instead of magic numbers
+		other_players_label[id] = nameLabel
+		add_child(nameLabel)
+		
 
+func get_username_from_id(id):
+	var regex = RegEx.new()
+	var username
+	regex.compile(pattern)
+	var match = regex.search(id)
+	if match:
+		username = match.get_string(1)
+	else:
+		username = id
+	
 
 func _on_login_change_to_zone_scene():
 	var zone = zone_scene.instantiate()
@@ -50,6 +70,7 @@ func _on_login_change_to_zone_scene():
 	print(get_children())
 	print(zone.change_scene)
 	zone.change_scene.connect(_mi_metodo_handler)
+	
 	
 func _mi_metodo_handler(mensaje):
 	print(mensaje)
