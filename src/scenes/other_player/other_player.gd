650ba1f311f4c5172fae95837e4c5b3b9d73538e
nravesz <nraveszani@gmail.com>
2023-12-14 18:31:31 -0300

    feature (ZOne): little tilemap

--- src/scenes/other_player/other_player.gd
+++ src/scenes/other_player/other_player.gd
@@ -7,7 +7,9 @@ var player_name = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Name.text = "[center][color=#ffaaaa]" + player_name + "[/color][/center]"
+	#$Name.text = "[center][color=#ffaaaa]" + player_name + "[/color][/center]"
+	pass
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
