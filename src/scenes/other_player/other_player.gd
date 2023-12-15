06e876d752f826ca3220592a501e7e951e154ebe
nravesz <nraveszani@gmail.com>
2023-12-15 14:30:29 -0300

    fix (other player): removes the color comment

--- src/scenes/other_player/other_player.gd
+++ src/scenes/other_player/other_player.gd
@@ -7,8 +7,7 @@ var player_name = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#$Name.text = "[center][color=#ffaaaa]" + player_name + "[/color][/center]"
-	pass
+	$Name.text = "[center][color=#ffaaaa]" + player_name + "[/color][/center]"
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
