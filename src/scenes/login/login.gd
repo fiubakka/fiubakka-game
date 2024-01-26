8741c9fe448a2c34534b0269600b6c43c3963ec5
FScaccheri <fscaccheri@gmail.com>
2024-01-25 22:31:24 -0300

    Fix player logging in with empty username

--- src/scenes/login/login.gd
+++ src/scenes/login/login.gd
@@ -6,6 +6,8 @@ signal user_logged_in(username: String)
 
 
 func _on_login_username_text_submitted(username: String) -> void:
+	if username.is_empty():
+		return
 	timer = Timer.new()  #This timer is to send the init message until we get a response from the server
 	timer.timeout.connect(Callable(self, "_on_timer_timeout").bind(username))
 	add_child(timer)
