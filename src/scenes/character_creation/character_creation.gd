8377460149450adf86ed4a4e9ca35604a9667bc7
Franco Scaccheri <fscaccheri@gmail.com>
2024-01-28 21:10:10 -0300

    Make MainMenu buttons work. Add return button for Login and CharCreation scenes

--- src/scenes/character_creation/character_creation.gd
+++ src/scenes/character_creation/character_creation.gd
@@ -1,6 +1,11 @@
 extends Control
 
 signal save_character
+signal return_to_menu
 
 func _on_button_pressed() -> void:
 	save_character.emit()
+
+
+func _on_return_return_to_menu() -> void:
+	return_to_menu.emit()
