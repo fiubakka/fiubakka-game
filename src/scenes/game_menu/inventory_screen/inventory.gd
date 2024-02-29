d5601c45020ba5b0477d81a0c658a106bb03cb42
nravesz <nraveszani@gmail.com>
2024-02-29 18:05:24 -0300

    feature: you can add a slot through the code in a scrolleable ocontainer

--- src/scenes/game_menu/inventory_screen/inventory.gd
+++ src/scenes/game_menu/inventory_screen/inventory.gd
@@ -1,7 +1,9 @@
 extends Control
 
+@export var inventory_slot_scene: PackedScene
+
 @export var Inventory: Array[InventoryItemData]
-@onready var slots: Array = $VBoxContainer/GridContainer.get_children()
+#@onready var slots: Array = $VBoxContainer/GridContainer.get_children()
 
 @onready var dni: InventoryItemData = preload(
 	"res://src/scenes/game_menu/inventory_screen/items/item_dni.tres"
@@ -18,16 +20,24 @@ func _ready() -> void:
 	#hat.texture.set_atlas(cs.hats_spritesheet[3])
 	#Inventory.append(hat)
 	var ic := ItemsCatalogue
-	print(ic.items_catalogue[1].texture)
+	#print(ic.items_catalogue[1].texture)
 	var sprite := $CharacterSprite/Hats
-	sprite.texture = ic.items_catalogue[1].texture.get_atlas()
-	Inventory.append(ic.items_catalogue[1])
+	#sprite.texture = ic.items_catalogue[0].texture.get_atlas()
+	Inventory.append(ic.items_catalogue[0])
+	Inventory.append(ic.items_catalogue[0])
+	var slots := $ScrollContainer/GridContainer
 	for i in range(0, len(Inventory)):
-		slots[i].update(Inventory[i])
-		slots[i].pressed.connect(self._on_Slot_Pressed.bind(Inventory[i]))
+		var slot := inventory_slot_scene.instantiate()
+		slot.update(Inventory[i])
+		slot.pressed.connect(self._on_Slot_Pressed.bind(Inventory[i]))
+		slots.add_child(slot)
+		#var foo := slots.get_children()
+		#foo[0].update(Inventory[0])
+		
 
 
 func _on_Slot_Pressed(item: InventoryItemData) -> void:
+	print("oressed")
 	if item:
 		var name: RichTextLabel = $VBoxContainer/Panel/Description/VBoxContainer/Name
 		name.add_text(item.name)
--- src/scenes/game_menu/inventory_screen/inventory_slot.gd
+++ src/scenes/game_menu/inventory_screen/inventory_slot.gd
@@ -1,10 +1,13 @@
 extends TextureButton
 
+class_name InventorySlot
 
 func update(item: InventoryItemData) -> void:
+	#var sprite: Sprite2D = $CenterContainer/Panel/Sprite2D
 	var sprite: Sprite2D = $CenterContainer/Panel/Sprite2D
 	sprite.texture = item.texture
 	print(sprite.texture)
+	print(sprite.texture == null)
 	var availableSize: Vector2 = size * Vector2(0.7, 0.7)
 	var scaleValue: float
 	if item.texture.get_size().x > item.texture.get_size().y:
