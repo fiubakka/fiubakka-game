0c7d58991a72031773b5b4fd4f6b43b2e0fca257
FScaccheri <fscaccheri@gmail.com>
2024-04-14 22:18:23 -0300

    Add typewritter effect to dialogue text

--- src/scenes/GUI/dialogue/dialogue.gd
+++ src/scenes/GUI/dialogue/dialogue.gd
@@ -2,11 +2,13 @@ extends Control
 
 @onready var label := $RichTextLabel
 @onready var timer := $Timer
-
+@onready var tween: Tween
 
 func show_tip(message: String) -> void:
+	tween = create_tween()
 	label.text = message
 	visible = true
+	tween.tween_property(label, "visible_ratio", 1.0, 1.0).from(0.0)
 	timer.start()
 
 
