b3ce2537f817ad29eefe27fc1f38ce48964d5187
nravesz <nraveszani@gmail.com>
2024-04-20 20:33:18 -0300

    instead of showing 30 matches, it shows 15 and if there's more than 15 points they start to turn golden

--- src/scenes/truco/Points/matches.gd
+++ src/scenes/truco/Points/matches.gd
@@ -2,15 +2,27 @@ extends Node2D
 
 class_name Matches
 
+var neutral: Color = Color(1, 1, 1)
+var golden: Color = Color(255, 215, 0)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	set_points(0)
+	set_points(0, false)
 
 
-func set_points(n: int) -> void:
+func set_points(n: int, is_golden: bool) -> void:
 	if n <= 5:
 		var children := get_children()
-		for i in range(0, n):
-			children[i].visible = true
-		for j in range(n, 5):
-			children[j].visible = false
+		if not is_golden:
+			for i in range(0, n):
+				children[i].visible = true
+				children[i].self_modulate = neutral
+			for j in range(n, 5):
+				children[j].visible = false
+		else:
+			for i in range(0, n):
+				children[i].visible = true
+				children[i].self_modulate = golden
+			for j in range(n, 5):
+				children[j].visible = true
+				children[j].self_modulate = neutral
