6b26f2204c29038311fa12285da66caf799c5bac
Ivan Soriano <ivan.soriano@live.com.ar>
2024-04-21 17:47:04 -0300

    Use only one node for error message texts

--- src/objects/utils/player_init_error_message_map.gd
+++ src/objects/utils/player_init_error_message_map.gd
@@ -3,7 +3,9 @@ extends Node
 const ERROR_CODES_TO_MSG_MAP := {
 	"UNKNOWN": "Error desconocido",
 	"INVALID_PLAYER_CREDENTIALS": "Credenciales invalidas",
-	"PLAYER_ALREADY_EXISTS": "Usuario no disponible"
+	"PLAYER_ALREADY_EXISTS": "Usuario no disponible",
+	"EMPTY_USERNAME": "Ingrese un nombre",
+	"EMPTY_PASSWORD": "Ingrese una contraseña",
 }
 
 
--- src/scenes/login/login.gd
+++ src/scenes/login/login.gd
@@ -24,16 +24,23 @@ func show_error_message(errorCode: String) -> void:
 
 
 func _on_button_pressed() -> void:
-	$NinePatchRect/VBoxContainer/Username/UsernameErrorText.visible = false
-	$NinePatchRect/VBoxContainer/Password/PasswordErrorText.visible = false
+	$NinePatchRect/VBoxContainer/LoginButtonBorder/LoginErrorText.visible = false
 	var username: String = $NinePatchRect/VBoxContainer/Username/LoginUsername.text
 	if username.is_empty():
-		$NinePatchRect/VBoxContainer/Username/UsernameErrorText.visible = true
+		var errorMessage: String = PlayerInitErrorMessageMap.error_code_to_msg("EMPTY_USERNAME")
+		$NinePatchRect/VBoxContainer/LoginButtonBorder/LoginErrorText.text = (
+			"[center]" + errorMessage + "[/center]"
+		)
+		$NinePatchRect/VBoxContainer/LoginButtonBorder/LoginErrorText.visible = true
 		return
 
 	var password: String = $NinePatchRect/VBoxContainer/Password/LoginPassword.text
 	if password.is_empty():
-		$NinePatchRect/VBoxContainer/Password/PasswordErrorText.visible = true
+		var errorMessage: String = PlayerInitErrorMessageMap.error_code_to_msg("EMPTY_PASSWORD")
+		$NinePatchRect/VBoxContainer/LoginButtonBorder/LoginErrorText.text = (
+			"[center]" + errorMessage + "[/center]"
+		)
+		$NinePatchRect/VBoxContainer/LoginButtonBorder/LoginErrorText.visible = true
 		return
 
 	timer = Timer.new()  # Timer to send init message until we get a response
@@ -41,7 +48,6 @@ func _on_button_pressed() -> void:
 	add_child(timer)
 	timer.set_wait_time(2.0)
 	timer.start()
-	#TODO: Stop timer once we receive response or return back to menu
 
 
 func _on_timer_timeout(username: String, password: String) -> void:
@@ -50,4 +56,6 @@ func _on_timer_timeout(username: String, password: String) -> void:
 
 func _on_return_return_to_menu() -> void:
 	$NinePatchRect/VBoxContainer/Username/LoginUsername.clear()
+	$NinePatchRect/VBoxContainer/Password/LoginPassword.clear()
+	timer.stop()
 	return_to_menu.emit()
