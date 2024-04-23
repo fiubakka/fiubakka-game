c97bf81214009f559c13ffbcf69beaf025d50a24
nravesz <nraveszani@gmail.com>
2024-04-23 17:44:26 -0300

    game over screen

---
+++ src/scenes/truco/GameOver/game_over.gd
@@ -0,0 +1,19 @@
+extends Control
+
+var text: RichTextLabel = null
+var confetti: Confetti = null
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	text = $CenterContainer/NinePatchRect/VBoxContainer/RichTextLabel
+	confetti = $CenterContainer/Panel/Confetti
+	set_victory()
+
+func set_victory() -> void:
+	text.set_text(Utils.center_text("Victory!"))
+	confetti.start()
+
+
+func set_defeat() -> void:
+	text.set_text(Utils.center_text("Defeat!"))
