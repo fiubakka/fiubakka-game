b84a9d7f459facc355f0ea113978c9dd32db4e33
Ivan Soriano <ivan.soriano@live.com.ar>
2024-04-29 19:39:09 -0300

    Fix linter

--- src/scenes/language_select/language_select.gd
+++ src/scenes/language_select/language_select.gd
@@ -4,19 +4,22 @@ signal go_to_main_menu
 
 const button_scene = preload("res://src/scenes/truco/Interface/button.tscn")
 
+
 func _ready() -> void:
 	var lang_container := $NinePatchRect/VBoxContainer/Languages
-	
+
 	var locales := TranslationServer.get_loaded_locales()
 	for locale in locales:
 		var button := _create_lang_button(locale)
 		lang_container.add_child(button)
+
 
 func _create_lang_button(locale: String) -> Button:
 	var button := button_scene.instantiate()
 	button.text = locale.to_upper()
 	button.pressed.connect(self._on_button_pressed.bind(button))
 	return button
+
 
 func _on_button_pressed(button: Button) -> void:
 	var locale: String = button.text.to_lower()
