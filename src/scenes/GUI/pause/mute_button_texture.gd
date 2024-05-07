5fcb5989eaa34e61cd0665802e4a5aca8309b4c4
Ivan Soriano <ivan.soriano@live.com.ar>
2024-05-07 15:41:53 -0300

    Fix texture rect limits

--- src/scenes/GUI/pause/mute_button_texture.gd
+++ src/scenes/GUI/pause/mute_button_texture.gd
@@ -6,8 +6,8 @@ var is_muted := false
 func _on_button_pressed() -> void:
 	is_muted = !is_muted
 	if is_muted:
-		var new_region := Rect2(1970, 774, 47, 42)
+		var new_region := Rect2(1968, 768, 48, 48)
 		region_rect = new_region
 	else:
-		var new_region := Rect2(1920, 774, 47, 42)
+		var new_region := Rect2(1920, 768, 48, 48)
 		region_rect = new_region
