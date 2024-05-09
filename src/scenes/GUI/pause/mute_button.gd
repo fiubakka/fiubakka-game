3b2bba654357242d9c9c3b4778c12d11852ce77d
Ivan Soriano <ivan.soriano@live.com.ar>
2024-05-09 19:44:28 -0300

    Add new music

--- src/scenes/GUI/pause/mute_button.gd
+++ src/scenes/GUI/pause/mute_button.gd
@@ -4,7 +4,6 @@ var is_muted := false
 
 
 func _on_pressed() -> void:
-	print("MUTING")
 	var bus_idx := AudioServer.get_bus_index("MusicMain")
 	AudioServer.set_bus_mute(bus_idx, !is_muted)
 	is_muted = !is_muted
