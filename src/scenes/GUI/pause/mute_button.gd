e9ddbd1ea81c4d0bf7da733d72903aea46b7521f
Ivan Soriano <ivan.soriano@live.com.ar>
2024-05-09 20:28:36 -0300

    Add sound manager to habdle mute

--- src/scenes/GUI/pause/mute_button.gd
+++ src/scenes/GUI/pause/mute_button.gd
@@ -1,9 +1,10 @@
 extends Button
 
+signal mute_toggled
+
 var is_muted := false
 
 
 func _on_pressed() -> void:
-	var bus_idx := AudioServer.get_bus_index("MusicMain")
-	AudioServer.set_bus_mute(bus_idx, !is_muted)
-	is_muted = !is_muted
+	SoundManager.toggle_mute()
+	mute_toggled.emit()
--- src/scenes/GUI/pause/mute_button_texture.gd
+++ src/scenes/GUI/pause/mute_button_texture.gd
@@ -1,10 +1,8 @@
 extends NinePatchRect
 
-var is_muted := false
 
-
-func _on_button_pressed() -> void:
-	is_muted = !is_muted
+func _on_mute_toggled() -> void:
+	var is_muted: bool = SoundManager.is_muted()
 	if is_muted:
 		var new_region := Rect2(1968, 768, 48, 48)
 		region_rect = new_region
