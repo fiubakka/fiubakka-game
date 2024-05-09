e11188cb235f99e77bf4a00951e55d957be4b2b2
FScaccheri <fscaccheri@gmail.com>
2024-05-09 13:31:21 -0300

    Fix chatbox opacity. Remove unused code

--- src/scenes/GUI/chatbox/chatbox.gd
+++ src/scenes/GUI/chatbox/chatbox.gd
@@ -12,7 +12,6 @@ func _ready() -> void:
 	pass  # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if self.visible:
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
