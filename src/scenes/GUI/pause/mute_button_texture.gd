cd150813c128031aa355bc0359162fcd15450bd7
Ivan Soriano <ivan.soriano@live.com.ar>
2024-05-10 00:22:08 -0300

    Directly assign Rect

--- src/scenes/GUI/pause/mute_button_texture.gd
+++ src/scenes/GUI/pause/mute_button_texture.gd
@@ -4,8 +4,6 @@ extends NinePatchRect
 func _on_mute_toggled() -> void:
 	var is_muted: bool = SoundManager.is_muted()
 	if is_muted:
-		var new_region := Rect2(1968, 768, 48, 48)
-		region_rect = new_region
+		region_rect = Rect2(1968, 768, 48, 48)
 	else:
-		var new_region := Rect2(1920, 768, 48, 48)
-		region_rect = new_region
+		region_rect = Rect2(1920, 768, 48, 48)
