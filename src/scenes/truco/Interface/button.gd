a0f349747d9eb69bcf9570231fcf018e09adcd20
nravesz <nraveszani@gmail.com>
2024-06-08 20:17:12 -0300

    fix button's size in options so it doesn't cover the matches

--- src/scenes/truco/Interface/button.gd
+++ src/scenes/truco/Interface/button.gd
@@ -6,3 +6,7 @@ class_name TrucoButton
 func _on_pressed() -> void:
 	var audio_player := $AudioStreamPlayer
 	audio_player.play()
+
+
+func set_min_size(min_size: Vector2) -> void:
+	custom_minimum_size = min_size
