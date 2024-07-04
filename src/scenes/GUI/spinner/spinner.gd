24f310a3a5bae7a599d1ddf4f7c0c23136f589d6
Franco Scaccheri <fscaccheri@fi.uba.ar>
2024-07-04 20:12:01 -0300

    Add spinner for login and register buttons

---
+++ src/scenes/GUI/spinner/spinner.gd
@@ -0,0 +1,17 @@
+extends TextureProgressBar
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var tween := get_tree().create_tween().set_loops()
+	tween.tween_property(self, "radial_initial_angle", 360.0, 1.5).as_relative()
+
+
+func _on_character_creation_user_logged_in(
+	_username: String, _password: String, _equipment: Equipment
+	) -> void:
+	visible = true
+
+
+func _on_character_creation_register_error(_errorCode: String) -> void:
+	visible = false
--- src/scenes/character_creation/register_button.gd
+++ src/scenes/character_creation/register_button.gd
@@ -2,13 +2,13 @@ extends Button
 
 
 func _on_character_creation_register_error(_error_code: String) -> void:
-	disabled = false
+	visible = true
 
 
 func _on_character_creation_user_logged_in(
 	_username: String, _password: String, _equipment: Equipment
 ) -> void:
-	disabled = true
+	visible = false
 
 
 func _on_pressed() -> void:
